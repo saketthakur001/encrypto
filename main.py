5c368f367edf4076e8e0000b3ffd493d7b5fd84d
saket <saketthakur4797>
2022-10-30 13:53:50 +0530

    2nd commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+class BoxLayout_(BoxLayout):
+    pass
+
+class MainScreen(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class main(App):
+    pass
+
+main().run()
